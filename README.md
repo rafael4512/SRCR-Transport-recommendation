8e40da582ee40e359b45c3bb3f0c2b758e978968
ril <rafaelinacio4512@gmail.com>
2020-05-21 02:58:40 +0100

    import da Base de conheciamento

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SRCR-Transport-recommendation
  
+##SWI
+compile("/Users/ril/github/SRCR-Transport-recommendation/auxiliar.pl").
+use_module(library(csv)).
---
+++ auxiliar.pl
@@ -0,0 +1,25 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Funções auxiliares
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Listas
+
+% Verifica se um elemento existe na lista.
+containsList(X, [],nao).
+containsList(X, [X|T] ,sim). 
+containsList(X, [H|T] ,R) :- containsList(X,T,R). 
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Criação da base de conhecimento.
+%
+% Exemplo de execucao 
+% criarBC("/Users/ril/github/SRCR-Transport-recommendation/Data.csv","/Users/ril/github/SRCR-Transport-recommendation/baseDeConhecimento2.pl",R).
+
+criarBC(Name_Exel,DirDestino,criada_Com_Sucesso) :- csv_read_file(Name_Exel,Data,[functor(viajar),arity(11),separator(0';)]), %'
+									open(DirDestino,write,OS),
+									mapBC(OS,Data,sucesso), 	
+								   	close(OS).
+
+mapBC(OS,[],sucesso).
+mapBC(OS,[Y|YS],R):-write(OS,Y),nl(OS), mapBC(OS,YS,R).
