f68c4e192bbe157272edb45d7c71f9e015efa530
ril <rafaelinacio4512@gmail.com>
2020-05-21 12:50:19 +0100

    criacao da main

--- auxiliar.pl
+++ auxiliar.pl
@@ -1,6 +1,6 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Funções auxiliares
-
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Listas
 
@@ -12,14 +12,20 @@ containsList(X, [H|T] ,R) :- containsList(X,T,R).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Criação da base de conhecimento.
-%
+% :- use_module(library(csv)) 
 % Exemplo de execucao 
 % criarBC("/Users/ril/github/SRCR-Transport-recommendation/Data.csv","/Users/ril/github/SRCR-Transport-recommendation/baseDeConhecimento2.pl",R).
 
-criarBC(Name_Exel,DirDestino,criada_Com_Sucesso) :- csv_read_file(Name_Exel,Data,[functor(viajar),arity(11),separator(0';)]), %'
+criarBC(Name_Exel,DirDestino,criada_Com_Sucesso) :- csv_read_file(Name_Exel,Data,[functor(paragem),arity(11),separator(0';)]), %'
 									open(DirDestino,write,OS),
 									mapBC(OS,Data,sucesso), 	
 								   	close(OS).
 
 mapBC(OS,[],sucesso).
 mapBC(OS,[Y|YS],R):-write(OS,Y),nl(OS), mapBC(OS,YS,R).
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+
+
+
